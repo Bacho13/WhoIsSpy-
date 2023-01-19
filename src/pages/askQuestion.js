6ebdc58de1108e55d0cb28766b54bb5e2694b893
Bacho <bacholashkhia13@gmail.com>
2023-01-19 14:12:41 +0400

    restart buton  will now restart game with same names and if needed to re-enter names have to click home button

--- src/pages/askQuestion.js
+++ src/pages/askQuestion.js
@@ -1,17 +1,28 @@
 import React from "react";
 import styles from "../styles/AskQuestions.module.scss";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
+import HomeIcon from "@mui/icons-material/Home";
 import Link from "next/link";
-
 function AskQuestion() {
-  //   const router = useRouter();
   return (
     <>
       <div className={styles.Main}>
+        <div className={styles.homeiconCont}>
+          <Link href="/" className={styles.link}>
+            <HomeIcon
+              className={styles.homeIcon}
+              style={{ width: "70px", height: "70px" }}
+            />
+          </Link>
+        </div>
+
         <p>დაუსვით კითხვები ერთმანეთს</p>
         <div className={styles.iconCont}>
-          <Link href="/">
-            <AutorenewIcon className={styles.icon} />
+          <Link href="/showHide" className={styles.iconLink}>
+            <AutorenewIcon
+              className={styles.customIcon}
+              style={{ width: "70px", height: "70px" }}
+            />
           </Link>
         </div>
       </div>
